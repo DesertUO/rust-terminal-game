44976874b6fee988fda11f29c9349d93fbd97472
DesertUO <desert21original@gmail.com>
2026-07-06 20:07:33 -0500

    Oops, forgot to remove the ansi characters, also kind of new pretty renderer? idk

--- src/main.rs
+++ src/main.rs
@@ -36,21 +36,25 @@ fn render_game(game: &Game, stdout: &mut BufWriter<std::io::Stdout>, count_tmp: 
 
     queue!(stdout, MoveTo(0,0)).map_err(|e| e.to_string())?;
 
-    let title = format!(" counter {} ", count_tmp.to_string());
+    let title = format!(" RTG ");
     let top_border = format!("┌{}┐\n", center_string_with_width_given_filler(title, (game.grid.width) as usize, '─'));
     queue!(stdout, Print(top_border), Clear(ClearType::UntilNewLine)).map_err(|e| e.to_string())?;
 
-    // print!("┌{}┐\x1b[K\n", "─".repeat(game.grid.width as usize));
     let test_height = game.grid.height as usize;
     for j in 0..test_height {
-        let formatted_row = format!("│{}│\x1b[K\n", data[j]);
+        let formatted_row = format!("│{}│\n", data[j]);
         queue!(stdout, Print(formatted_row), Clear(ClearType::UntilNewLine)).map_err(|e| e.to_string())?;
     }
 
-    let bottom_border = format!("└{}┘\x1b[K\n", "─".repeat(game.grid.width as usize));
+    let bottom_border = format!("└{}┘\n", "─".repeat(game.grid.width as usize));
     queue!(stdout, Print(bottom_border), Clear(ClearType::UntilNewLine)).map_err(|e| e.to_string())?;
 
-    queue!(stdout, Print("Press Esq to exit."), Clear(ClearType::UntilNewLine)).map_err(|e| e.to_string())?;
+    queue!(stdout, Print("Press Esq to exit.\n\n"), Clear(ClearType::UntilNewLine)).map_err(|e| e.to_string())?;
+
+    // Test
+    let counter_tmp_test = format!("counter {}", count_tmp.to_string());
+    queue!(stdout, Print(counter_tmp_test), Clear(ClearType::UntilNewLine)).map_err(|e| e.to_string())?;
+
 
     stdout.flush().map_err(|e| e.to_string())?;
 
